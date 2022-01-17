70545d219128efafd37ed57de4096df2dd433ccb
Aitor Garcia Diez <bbminze@gmail.com>
2022-01-17 17:50:28 +0100

    added g8 entity conf file for split conf files

--- src/main/scaffolds/entity/rest/app/$package__lower,package,packaged$/rest/controllers/$entity__Camel$Controller.scala
+++ src/main/scaffolds/entity/rest/app/$package__lower,package,packaged$/rest/controllers/$entity__Camel$Controller.scala
@@ -1,12 +1,15 @@
 package $package;format="lower,package"$.rest
 package controllers
 import io.github.minze25.scalamediator.core.IMediator
-import play.api.mvc.{BaseController, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, BaseController, ControllerComponents}
 import javax.inject.{Inject, Singleton}
 
 @Singleton
 class $entity;format="Camel"$Controller @Inject()
 (val controllerComponents: ControllerComponents, val mediator: IMediator)
   extends BaseController {
-// Don't forget to add the methods to the conf/routes file
+// Don't forget to add the methods to the conf/$entity;format="lower"$routes file
+  def index: Action[AnyContent] = Action {
+    NoContent
+  }
 }
