1e718e228132137d02e8c5d9f27fb9eabe4b59b0
Aitor Garcia Diez <bbminze@gmail.com>
2022-01-21 16:07:33 +0100

    Adding EntityDTO and EntityRepository when scaffolding gen entity;

---
+++ src/main/scaffolds/entity/application/src/main/scala/$package__lower,package,packaged$/$project__lower,word$/application/$entity__lower$/$entity__Camel$Repository.scala
@@ -0,0 +1,4 @@
+package $package;format="lower,package"$.$project;format="lower,word"$.application.$entity;format="lower,package"$
+import $package;format="lower,package"$.$project;format="lower,word"$.application.Repository
+abstract class $entity;format="Camel"$Repository extends Repository[$entity;format="Camel"$DTO] {
+}
